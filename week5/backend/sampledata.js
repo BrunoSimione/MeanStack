ddbe05a230d964338cf74c42da476cbb8d041455
Bruno Simione Beltrame <bruno.simione@gmail.com>
2019-02-22 17:04:23 -0500

    New files

--- week5/backend/sampledata.js
+++ week5/backend/sampledata.js
@@ -1,80 +1,44 @@
     const MongoClient = require("mongodb").MongoClient;
-    const DBName = "entertainment";
+    const DBName = "products";
 
-    //note authentication via url
-    //this should be replaced with environment variables (for security)
     const url = `mongodb://192.168.99.100:27017/${DBName}`;
 
-    /*
-    Note:
-    This example assumes mongo running in a Docker container, from a standard docker mongo image.
-    docker run --name my-mongo -it -p 27017:27017 mongo:3.4.18-jessie
 
-    Use 'docker-machine env' to figure out the IP of your host network, and be sure to forward port 27017
-    */
-
-    const actorData = [
+    const eletronicsData = [
       {
-        firstName: "John",
-        lastName: "Travolta",
-        birth: "February 18, 1954",
-        credits: 76,
-        tags: ["actor", "soundtrack", "producer"],
-        image:
-          "https://images-na.ssl-images-amazon.com/images/M/MV5BMTUwNjQ0ODkxN15BMl5BanBnXkFtZTcwMDc5NjQwNw@@._V1_SY1000_CR0,0,743,1000_AL_.jpg",
-        movies: [
-          {
-            title: "Battlefield Earth",
-            year: 2000
-          },
-          {
-            title: "FaceOff",
-            year: 1997
-          },
-          {
-            title: "Pulp Fiction",
-            year: 1994
-          }
-        ]
+        brand: "Samsung",
+        price: 1200.00,
+        model: "TV 50 Turbo",
+        tags: ["TV", "LG", "SMARTV"],
+        description: {
+            height: "80cm",
+            weight: "120cm"
+        }
       },
       {
-        firstName: "Uma",
-        lastName: "Thurman",
-        birth: "April 29, 1970",
-        tags: ["actress", "soundtrack", "writer"],
-        image:
-          "https://images-na.ssl-images-amazon.com/images/M/MV5BMjMxNzk1MTQyMl5BMl5BanBnXkFtZTgwMDIzMDEyMTE@._V1_SY1000_CR0,0,665,1000_AL_.jpg",
-        credits: 63,
-        awards: [
-          {
-            type: "oscar",
-            status: "nominated",
-            title: "best actress",
-            year: 1995,
-            film: "Pulp Fiction"
-          },
-          {
-            type: "golden globe",
-            status: "nominated",
-            title: "best performance",
-            year: 2005,
-            film: "Kill Bill Vol2"
-          }
-        ],
-        movies: [
-          {
-            title: "Kill Bill Vol1",
-            year: 2003
-          },
-          {
-            title: "Pulp Fiction",
-            year: 1994
-          },
-          {
-            title: "Batman and Robin",
-            year: 1994
-          }
-        ]
+        brand: "LG",
+        price: 750.10,
+        model: "Soundbar XY4561",
+        tags: ["SOUNDBAR", "LG"],
+        description: {
+            height: "20cm",
+            weight: "75cm",
+			depth: "20cm",
+			watts: "50"
+        }
+      },
+	  {
+        brand: "Sony",
+        price: 850.20,
+        model: "Home Theater",
+        tags: ["HOME THEATER", "SONY", "SOUND"],
+        description: {
+            height: "20cm",
+            weight: "50cm",
+			depth: "20cm",
+			watts: "75"
+        },
+		warranty:"2 Years"
       }
     ];
 
@@ -84,9 +48,9 @@
         // <- callback
         console.log("Connected");
         return con
-          .db("entertainment")
-          .collection("actors")
-          .insert(actorData)
+          .db("products")
+          .collection("eletronics")
+          .insert(eletronicsData)
           .then(out => console.log(out))
           .then(() => con.close());
       })
