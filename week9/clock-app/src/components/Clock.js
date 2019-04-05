ce8a1c2668438123f14c086963a5decbe90a0033
Bruno Simione Beltrame <bruno.simione@gmail.com>
2019-04-05 11:49:07 -0400

    Style + Toggle

--- week9/clock-app/src/components/Clock.js
+++ week9/clock-app/src/components/Clock.js
@@ -1,15 +1,27 @@
 import React from "react";
+import {Button} from "@material-ui/core";
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
+
+const styles = theme => ({
+  button: {
+    margin: theme.spacing.unit,
+  },
+});
+
 const Clock = props => {
+  console.log(props);
+  const { classes } = props;
   if (props.date) {
     return (
       <div>
         <h1> The time is {` ${props.date}`}</h1>
-        <button onClick={props.action}>Stop Click</button>
-        <button onClick={props.action2}>Reset</button>
+        <Button  className={classes.button} variant="contained" color="secondary" onClick={props.action}>Start / Stop Click</Button>
       </div>
     );
   } else {
     return <h1>Loading</h1>;
   }
 };
-export default Clock;
+
+export default withStyles(styles)(Clock);
